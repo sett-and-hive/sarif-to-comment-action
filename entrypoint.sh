a26e0e6cdb9f4433b0aaf80779860d5b1a64bff8
Tom Willis <twillis@flexion.us>
2022-05-27 15:01:26 -0500

    clean up entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,6 @@ set -o pipefail
 set -exu
 set -C
 
-pwd
-ls -latR test
-
 echo "Convert SARIF file $1"
 npx @security-alert/sarif-to-comment --dryRun "$7" --token "$2" --commentUrl "$3" --sarifContentOwner "$4" --sarifContentRepo "$5" --sarifContentBranch "$6" --title "ODC SARIF vulnerabilities report" --ruleDetails true --suppressedResult true "$1"
 echo "::set-output name=output::$?"
