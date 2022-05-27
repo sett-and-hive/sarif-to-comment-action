e1c6c8444b1384cf70f32045f7c6c97c7b2c75a1
Tom Willis <twillis@flexion.us>
2022-05-26 20:38:02 -0500

    CI test action

--- test/test.sh
+++ test/test.sh
@@ -5,19 +5,20 @@
 docker build . -t comment
 export DRY_RUN="true"
 export LIVE_RUN="false"
-MODE=$LIVE_RUN
-docker run --rm -v "$(pwd)/test":/app/test comment test/fixtures/codeql.sarif fake-password https://github.com/tomwillis608/sarif-to-comment-action/pull/1 tomwillis608 sarif-to-comment-action fake-test-branch $MODE 2>&1 | tee test/results.txt
+MODE=$DRY_RUN
+OUTPUTS_FILE=test/test-outputs.txt
+docker run --rm -v "$(pwd)/test":/app/test comment test/fixtures/codeql.sarif fake-password https://github.com/tomwillis608/sarif-to-comment-action/pull/1 tomwillis608 sarif-to-comment-action fake-test-branch $MODE 2>&1 | tee "$OUTPUTS_FILE"
 if [ "$MODE" = "$DRY_RUN" ]; then
   TEST_STRING="DryRun results:"
 else
   TEST_STRING="HttpError: Bad credentials"
 fi
-if grep -Fxq "$TEST_STRING" test/results.txt; then
+if grep -Fxq "$TEST_STRING" "$OUTPUTS_FILE"; then
   echo
-  echo "Test passes"
+  echo "✅ Test result: passes"
 
 else
   echo
-  echo "Test fails"
+  echo "❌ Test result: fails"
   exit 1
 fi
